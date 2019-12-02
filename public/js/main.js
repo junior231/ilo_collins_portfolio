eac11b8a14a2ace445ed23baa15717292365fa38
Ilo <c_ilo@ats-hr420-dm34.fc.ca>
2019-12-02 17:58:06 -0500

    folder structure, codes 40%

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+console.log('JS linked.');
+
+gsap.to(".logo", {duration: 2, x:20, ease: "bounce"});
+gsap.to(".loader", {duration: 5, x:40, y:30});
+gsap.to(".hand", {duration: 2, rotation: 360, ease: "bounce"});
+
+const burger  = document.querySelector('.burger'),
+      bLine1  = document.querySelector('.line2'),
+      bLine2  = document.querySelector('.line3'),
+      nav     = document.querySelector('.headNav'),
+      header  = document.querySelector('.mainHeader');
+      playVideo = document.querySelector('#play'),
+      lightBox = document.querySelector('.lightbox'),
+      
+      lightboxControls = document.querySelectorAll('.open-lightbox');
+
+      function openNav(){
+        console.log('Nav toggled.');
+
+        bLine1.classList.toggle('open');
+        bLine2.classList.toggle('open2');
+        nav.classList.toggle('open');
+    }
+
+	function showLightbox(e) {
+		e.preventDefault();
+		hideLightbox ();
+		// this.nextElementSibling.classList.add('display');
+	}
+
+	function hideLightbox() {
+		document.querySelectorAll('.lightbox').forEach(lightbox => lightbox.classList.remove('display'));
+	}
+    
+    if (lightboxControls) {
+		lightboxControls.forEach(button => button.addEventListener("click", showLightbox));
+    }
+    
+burger.addEventListener('click', openNav);
