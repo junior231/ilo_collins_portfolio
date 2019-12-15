61c5f7c55a9d6add3443c350df108d9941632901
junior231 <43250479+junior231@users.noreply.github.com>
2019-12-14 21:38:52 -0500

    edited port

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
   
-// const config = {
-//     host: "us-cdbr-iron-east-05.cleardb.net",
-//     port: 3306, 
-//     uname: "bbd29ef3bcf34a",
-//     pword: "e7e72af3", 
-//     database: "heroku_acd4f74e5cdd94a"
-// }
-
-// module.exports = config;
-
-
 const config = {
-   host: "localhost",
-   port: 3306, // 8889 for mac 3306 for windows
-   uname: "root",
-   pword: "", // leave blank for windows
-   database: "db_portfolio"
+    host: "us-cdbr-iron-east-05.cleardb.net",
+    port: 3306, 
+    uname: "bbd29ef3bcf34a",
+    pword: "e7e72af3", 
+    database: "heroku_acd4f74e5cdd94a"
 }
 
 module.exports = config;
+
+
+// const config = {
+//    host: "localhost",
+//    port: 3306, // 8889 for mac 3306 for windows
+//    uname: "root",
+//    pword: "", // leave blank for windows
+//    database: "db_portfolio"
+// }
+
+// module.exports = config;
